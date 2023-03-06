f0b19b09e1eb17674eaceac2c693b0a7f3bdcc5e
Zwoelf12 <65718465+Zwoelf12@users.noreply.github.com>
2023-03-06 17:30:39 +0100

    testing komo and scvx on the workstation

--- scripts/optimization/parameter_tuning.py
+++ scripts/optimization/parameter_tuning.py
@@ -7,9 +7,9 @@ class SCVX_parameter():
         if prob_name == "simple_flight_wo_obs":
 
             # user defined parameter
-            self.lam = 1e3  # weight 3 slack in cost
+            self.lam = 1e2  # weight 3 slack in cost
             self.alp = 0.  # weight for time in cost
-            self.bet = 1e3  # weight for input in cost
+            self.bet = 1e3*5  # weight for input in cost
             self.gam = 0.  # weight ratio between input and time penalty
             self.adapWeightsFac = 1e1  # determines by how much the slack cost is decreased and the problem cost is increased when slack is in a reasonable range
             self.weightsFac = 1
