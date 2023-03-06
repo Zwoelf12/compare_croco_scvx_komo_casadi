cef9ae7c05f9e7e21eacad36b2d0695c7d94ab6f
Zwoelf12 <65718465+Zwoelf12@users.noreply.github.com>
2023-03-06 16:04:24 +0100

    testing komo and scvx on the workstation

--- scripts/optimization/SCP/SCvx.py
+++ scripts/optimization/SCP/SCvx.py
@@ -25,7 +25,7 @@ class SCvx():
     parameter = SCVX_parameter(prob_name)
 
     # user defined parameter
-    self.lam = 1e5 # weight 3 slack in cost
+    self.lam = 1e3 # weight 3 slack in cost
     self.alp = 0.  # weight for time in cost
     self.bet = 1e3 # weight for input in cost
     self.gam = 0.  # weight ratio between input and time penalty
