380c4f9d40583bd125128f0d8d944fad2f7c3ef7
Zwoelf12 <65718465+Zwoelf12@users.noreply.github.com>
2023-03-06 17:31:38 +0100

    testing komo and scvx on the workstation

--- scripts/optimization/parameter_tuning.py
+++ scripts/optimization/parameter_tuning.py
@@ -9,7 +9,7 @@ class SCVX_parameter():
             # user defined parameter
             self.lam = 1e2  # weight 3 slack in cost
             self.alp = 0.  # weight for time in cost
-            self.bet = 1e3*5  # weight for input in cost
+            self.bet = 1e3  # weight for input in cost
             self.gam = 0.  # weight ratio between input and time penalty
             self.adapWeightsFac = 1e1  # determines by how much the slack cost is decreased and the problem cost is increased when slack is in a reasonable range
             self.weightsFac = 1
@@ -34,9 +34,9 @@ class SCVX_parameter():
         elif prob_name == "complex_flight_spheres":
 
             # user defined parameter
-            self.lam = 1e5  # weight 3 slack in cost
+            self.lam = 1e2  # weight 3 slack in cost
             self.alp = 0.  # weight for time in cost
-            self.bet = 1  # weight for input in cost
+            self.bet = 1e3  # weight for input in cost
             self.gam = 0.  # weight ratio between input and time penalty
             self.adapWeightsFac = 1e1  # determines by how much the slack cost is decreased and the problem cost is increased when slack is in a reasonable range
             self.weightsFac = 1
