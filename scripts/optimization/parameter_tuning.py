c1f208288a73f91d8e96b762a61ae771a91730ea
Zwoelf12 <65718465+Zwoelf12@users.noreply.github.com>
2023-03-06 17:36:57 +0100

    testing komo and scvx on the workstation

--- scripts/optimization/parameter_tuning.py
+++ scripts/optimization/parameter_tuning.py
@@ -34,7 +34,7 @@ class SCVX_parameter():
         elif prob_name == "complex_flight_spheres":
 
             # user defined parameter
-            self.lam = 1e2  # weight 3 slack in cost
+            self.lam = 1e3  # weight 3 slack in cost
             self.alp = 0.  # weight for time in cost
             self.bet = 1e3  # weight for input in cost
             self.gam = 0.  # weight ratio between input and time penalty
